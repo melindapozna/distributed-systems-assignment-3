8b675933c054d5cb84e08b8e96f6c17c63625b72
melindapozna <melindapozna@gmail.com>
2025-04-09 01:16:18 +0300

    Implement group and private messaging

--- client.py
+++ client.py
@@ -6,7 +6,6 @@ from random import randint
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 client.bind(("localhost", randint(1000, 5999)))
 
-
 def receive():
     try:
         message, _ = client.recvfrom(2048)
@@ -15,21 +14,66 @@ def receive():
         pass
         # TODO
 
+def send(nickname, recipient, message_type):
+    while True:
+        message = input()
+        if message == "/q":
+            print("Goodbye!")
+            client.close()
+            exit(0)
+        elif message == "/change":
+            recipient, message_type = change_message_type()
+        else:
+            client.sendto(f"{nickname}::{message}::{message_type}::{recipient}".encode(), ("localhost", 6000))
+
+
+def change_message_type():
+    while True:
+        print("""Change message type:
+    1. Private message
+    2. Groups
+    0. Quit
+        """)
+        try:
+            choice = int(input("Your choice:"))
+        except TypeError:
+            print("Invalid choice")
+            continue
+        if choice == 0:
+            print("Goodbye!")
+            client.close()
+            exit(0)
+        elif choice == 1:
+            return change_recipient("p")
+        elif choice == 2:
+            return change_recipient("g")
+
+
+def change_recipient(message_type):
+    recipient = input("Enter your recipient/channel:")
+    if recipient == "/q":
+        print("Goodbye!")
+        client.close()
+        exit(0)
+    return recipient, message_type
+
+
 
 if __name__ == "__main__":
-    thread = threading.Thread(target=receive)
-    thread.start()
+    thread_rcv = threading.Thread(target=receive)
+    thread_rcv.start()
 
     print("Type /q to quit")
+    print("Type /change to change message type")
+
     nickname = input("Enter your nickname:")
     if nickname == "/q":
         print("Goodbye!")
         exit(0)
 
-    while True:
-        message = input()
-        if message == "/q":
-            print("Goodbye!")
-            exit(0)
-        client.sendto(f"{nickname}: {message}".encode(), ("localhost", 6000))
+    change_message_type()
+
+    thread_send = threading.Thread(target=send)
+    thread_send.start()
+
 
--- server.py
+++ server.py
@@ -1,46 +1,82 @@
 import threading
 import socket
 import queue
+from socketserver import ThreadingMixIn
+from xmlrpc.server import SimpleXMLRPCServer
 
 # list of online clients
 clients = []
+nicknames_to_addresses = {}
+nicknames_to_addresses.setdefault("", "")
 
-messages = queue.Queue()
+groups = {}
+
+private_messages = queue.Queue()
+group_messages = queue.Queue()
 
 # init UDP socket
 server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server.bind(("localhost", 6000))
 
 
-def receive():
+def receive(client):
     try:
-        messages.put(server.recvfrom(2048))
+        message, address = server.recvfrom(2048)
+        message_attributes = message.decode().split("::")
+        nickname = message_attributes[0]
+        content = message_attributes[1]
+        message_type = message_attributes[2]    # g - group, p - private
+        recipient = message_attributes[3]
+
+        if address not in clients:
+            clients.append(address)
+
+        if message_type == "p":
+            private_messages.put((nickname, content, nicknames_to_addresses[recipient], address))
+            if not nickname in nicknames_to_addresses:
+                nicknames_to_addresses[nickname] = address
+
+        elif message_type == "g":
+            group_messages.put((nickname, content, groups[recipient], address))
+            if not groups[recipient]:
+                groups[recipient] = [address]
+            if address not in groups[recipient]:
+                groups[recipient].append(address)
+        print(message)
     except:
         pass
         # TODO
 
 
-# send to every client
-def broadcast():
+def send_private():
     while True:
-        while not messages.empty():
-            message, address = messages.get()
-            print(message.decode())
-            if address not in clients:
-                clients.append(address)
-            for client in clients:
-                try:
-                    if message.decode().startswith("nickname:"):
-                        nickname = message.decode()[message.decode().index(":") + 1:]
-                        server.sendto(f"{nickname} joined!".encode(), client)
-                    else:
-                        server.sendto(message, client)
-                except:
-                    clients.remove(client)
+        while not private_messages.empty():
+            nickname, content, recipient, address = private_messages.get()
+            try:
+                if address not in clients:
+                    pass
+                server.sendto(f"{nickname}: {content}".encode(), nicknames_to_addresses[recipient])
+            except:
+                clients.remove(nicknames_to_addresses[recipient])
+                nicknames_to_addresses.pop(recipient)
+
+def send_group():
+    while True:
+        while not group_messages.empty():
+            nickname, content, group_name, address = private_messages.get()
+            try:
+                if address not in clients:
+                    pass
+                for address in groups[group_name]:
+                    server.sendto(f"{group_name.upper()}/{nickname}: {content}".encode(), address)
+            except:
+                groups[group_name].remove(address)
 
 
-thread1 = threading.Thread(target=receive)
-thread2 = threading.Thread(target=broadcast)
+thread_receive = threading.Thread(target=receive)
+thread_private = threading.Thread(target=send_private)
+thread_group = threading.Thread(target=send_group)
 
-thread1.start()
-thread2.start()
+thread_receive.start()
+thread_private.start()
+thread_group.start()
